d0d963c5545088ee6802948ec83997bb76f33c07
mamer12 <46693079+mamer12@users.noreply.github.com>
2021-05-12 15:52:33 +0300

    prep

--- my_collage_proj/lib/main.dart
+++ my_collage_proj/lib/main.dart
@@ -1,18 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'SignIn.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: LoginScreen(),
-    );
-  }
-}
+void main() => runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: LoginPageWidget()));
