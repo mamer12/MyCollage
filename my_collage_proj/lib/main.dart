1a80238836be2ae84ef87b980cd0131a9b631ca6
mamer12 <46693079+mamer12@users.noreply.github.com>
2021-05-13 14:29:48 +0300

    update

--- my_collage_proj/lib/main.dart
+++ my_collage_proj/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'SignIn.dart';
-import 'facebook.dart';
 
 void main() => runApp(
-    MaterialApp(debugShowCheckedModeBanner: false, home: LoginWithFacebook()));
+    MaterialApp(debugShowCheckedModeBanner: false, home: LoginPageWidget()));
