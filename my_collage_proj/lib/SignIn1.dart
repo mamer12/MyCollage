c9f5c60931469611a371d2e97a3806e37883c301
mamer12 <46693079+mamer12@users.noreply.github.com>
2021-05-20 12:49:10 +0300

    Updates

--- my_collage_proj/lib/SignIn1.dart
+++ my_collage_proj/lib/SignIn1.dart
@@ -1,5 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter/services.dart';
+
+import 'SignIn.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -8,6 +14,32 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool _rememberMe = false;
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+  FirebaseAuth _auth;
+  bool _isLoggedIn = false;
+  Map _userObj = {};
+  bool isUserSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initApp();
+  }
+
+  void initApp() async {
+    FirebaseApp defaultApp = await Firebase.initializeApp();
+    _auth = FirebaseAuth.instanceFor(app: defaultApp);
+    checkIfUserIsSignedIn();
+  }
+
+  void checkIfUserIsSignedIn() async {
+    var userSignedIn = await _googleSignIn.isSignedIn();
+
+    setState(() {
+      isUserSignedIn = userSignedIn;
+    });
+  }
+
   final kHintTextStyle = TextStyle(
     color: Colors.white54,
     fontFamily: 'OpenSans',
@@ -37,37 +69,15 @@ class _LoginScreenState extends State<LoginScreen> {
         Container(
           padding: EdgeInsets.symmetric(vertical: 20.0),
           width: double.infinity,
-          child: OutlineButton.icons(
-            onPressed: () => print('Login Button Pressed'),
-            padding: EdgeInsets.all(15.0),
-            shape: StadiumBorder(),
-            textColor: Colors.white,
-            borderSide: BorderSide(
-                color: Colors.white, style: BorderStyle.solid, width: 4),
-            child: Text(
-              'LOGIN',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 1.5,
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'OpenSans',
-              ),
-            ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(vertical: 10.0),
-          width: double.infinity,
           child: OutlineButton(
-            onPressed: () => print('Login Button Pressed'),
+            onPressed: () => onGoogleSignIn(context),
             padding: EdgeInsets.all(15.0),
             shape: StadiumBorder(),
             textColor: Colors.white,
             borderSide: BorderSide(
                 color: Colors.white, style: BorderStyle.solid, width: 4),
             child: Text(
-              'LOGIN',
+              'LOGIN WITH Google',
               style: TextStyle(
                 color: Colors.white,
                 letterSpacing: 1.5,
@@ -80,6 +90,61 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ]),
     );
+  }
+
+  Future<User> _handleSignIn() async {
+    User user;
+    bool userSignedIn = await _googleSignIn.isSignedIn();
+
+    setState(() {
+      isUserSignedIn = userSignedIn;
+    });
+
+    if (isUserSignedIn) {
+      user = _auth.currentUser;
+    } else {
+      final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      user = (await _auth.signInWithCredential(credential)).user;
+      userSignedIn = await _googleSignIn.isSignedIn();
+      setState(() {
+        isUserSignedIn = userSignedIn;
+      });
+    }
+
+    return user;
+  }
+
+  void onGoogleSignIn(BuildContext context) async {
+    User user = await _handleSignIn();
+    var userSignedIn = await Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => WelcomeUserWidget(user, _googleSignIn)),
+    );
+
+    setState(() {
+      isUserSignedIn = userSignedIn == null ? true : false;
+    });
+  }
+
+  void onFaceBookSignIn(BuildContext context) async {
+    FacebookAuth.instance
+        .login(permissions: ["public_profile", "email"]).then((value) {
+      FacebookAuth.instance.getUserData().then((userData) {
+        setState(() {
+          _isLoggedIn = true;
+          _userObj = userData;
+        });
+      });
+    });
   }
 
   @override
